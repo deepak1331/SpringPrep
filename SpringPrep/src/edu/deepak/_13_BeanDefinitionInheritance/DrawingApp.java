459888030e638761a061126ca724444e01bc0a20
Deepak <deepak.yadav@jumprampgames.com>
2018-08-24 15:57:45 +0530

    BeanDefinitionInheritance:  Abstract="true" marks a bean as abstract bean

--- SpringPrep/src/edu/deepak/_13_BeanDefinitionInheritance/DrawingApp.java
+++ SpringPrep/src/edu/deepak/_13_BeanDefinitionInheritance/DrawingApp.java
@@ -8,11 +8,11 @@ public class DrawingApp {
 
 		ApplicationContext context = new ClassPathXmlApplicationContext("spring13.xml");
 
-//		Triangle triangle = (Triangle) context.getBean("triangle1");
-//		triangle.draw();
-//
-//		triangle = (Triangle) context.getBean("triangle2");
-//		triangle.draw();
+//		Triangle triangle1 = (Triangle) context.getBean("triangle1");
+//		triangle1.draw();
+
+		Triangle triangle2 = (Triangle) context.getBean("triangle2");
+		triangle2.draw();
 		
 		Polygon polygon = (Polygon) context.getBean("polygon1");
 		polygon.draw();
